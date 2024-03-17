4fd462927a2cb50442835199ed1a2f8a8b3dd194
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-03-17 16:28:10 +0530

    re-rendering the component with setState and updating UI

--- src/App.js
+++ src/App.js
@@ -8,11 +8,15 @@ export default function App() {
   // Passing function as prop to retrieve the search term from the SearchBar Component
   const [images, setImages] = useState([]);
 
-  const getTerm = (term) => {
+  const getTerm = async (term) => {
     console.log(`I am the App component and the term is ${term}`);
 
     //   Step-2: Get the images now!
-    searchImages(term);
+    const response = await searchImages(term);
+    console.log(response);
+
+    setImages(response);
+    // This will re-render the component and ImageList will pass the updated images to the ImageList component
   };
 
   return (
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,5 +1,7 @@
 import React from "react";
 
-export default function ImageList() {
+export default function ImageList({ images }) {
+  console.log("*******");
+  console.log(images);
   return <div>ImageList</div>;
 }
